bce0ed27337ee8efcaf9bdf41a2a19a36a90a79d
zone17 <matt.wollschleger@gmail.com>
2025-03-20 23:22:00 -0400

    Rearrange zodiac cards in calendar year order starting with Capricorn

--- src/components/HoroscopeDisplay.tsx
+++ src/components/HoroscopeDisplay.tsx
@@ -6,6 +6,9 @@ import { getHoroscopesForAllSigns } from '@/utils/horoscope-service';
 
 // Zodiac sign data with symbols, date ranges, and elements
 const ZODIAC_SIGNS = [
+  { sign: 'capricorn', symbol: '♑', dateRange: 'Dec 22 - Jan 19', element: 'Earth' },
+  { sign: 'aquarius', symbol: '♒', dateRange: 'Jan 20 - Feb 18', element: 'Air' },
+  { sign: 'pisces', symbol: '♓', dateRange: 'Feb 19 - Mar 20', element: 'Water' },
   { sign: 'aries', symbol: '♈', dateRange: 'Mar 21 - Apr 19', element: 'Fire' },
   { sign: 'taurus', symbol: '♉', dateRange: 'Apr 20 - May 20', element: 'Earth' },
   { sign: 'gemini', symbol: '♊', dateRange: 'May 21 - Jun 20', element: 'Air' },
@@ -15,9 +18,6 @@ const ZODIAC_SIGNS = [
   { sign: 'libra', symbol: '♎', dateRange: 'Sep 23 - Oct 22', element: 'Air' },
   { sign: 'scorpio', symbol: '♏', dateRange: 'Oct 23 - Nov 21', element: 'Water' },
   { sign: 'sagittarius', symbol: '♐', dateRange: 'Nov 22 - Dec 21', element: 'Fire' },
-  { sign: 'capricorn', symbol: '♑', dateRange: 'Dec 22 - Jan 19', element: 'Earth' },
-  { sign: 'aquarius', symbol: '♒', dateRange: 'Jan 20 - Feb 18', element: 'Air' },
-  { sign: 'pisces', symbol: '♓', dateRange: 'Feb 19 - Mar 20', element: 'Water' },
 ];
 
 export function HoroscopeDisplay() {
--- src/components/zodiac/HoroscopeDisplay.tsx
+++ src/components/zodiac/HoroscopeDisplay.tsx
@@ -11,6 +11,9 @@ import { useMode } from '@/hooks/useMode';
 
 // Zodiac sign data with symbols, date ranges, and elements
 const ZODIAC_SIGNS = [
+  { sign: 'capricorn', symbol: '♑', dateRange: 'Dec 22 - Jan 19', element: 'Earth' },
+  { sign: 'aquarius', symbol: '♒', dateRange: 'Jan 20 - Feb 18', element: 'Air' },
+  { sign: 'pisces', symbol: '♓', dateRange: 'Feb 19 - Mar 20', element: 'Water' },
   { sign: 'aries', symbol: '♈', dateRange: 'Mar 21 - Apr 19', element: 'Fire' },
   { sign: 'taurus', symbol: '♉', dateRange: 'Apr 20 - May 20', element: 'Earth' },
   { sign: 'gemini', symbol: '♊', dateRange: 'May 21 - Jun 20', element: 'Air' },
@@ -20,9 +23,6 @@ const ZODIAC_SIGNS = [
   { sign: 'libra', symbol: '♎', dateRange: 'Sep 23 - Oct 22', element: 'Air' },
   { sign: 'scorpio', symbol: '♏', dateRange: 'Oct 23 - Nov 21', element: 'Water' },
   { sign: 'sagittarius', symbol: '♐', dateRange: 'Nov 22 - Dec 21', element: 'Fire' },
-  { sign: 'capricorn', symbol: '♑', dateRange: 'Dec 22 - Jan 19', element: 'Earth' },
-  { sign: 'aquarius', symbol: '♒', dateRange: 'Jan 20 - Feb 18', element: 'Air' },
-  { sign: 'pisces', symbol: '♓', dateRange: 'Feb 19 - Mar 20', element: 'Water' },
 ];
 
 // Helper functions to get zodiac sign information
